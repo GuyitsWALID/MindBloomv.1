039d96e54803754d606aa05b4f1cf8f25cae4d2a
Walid Murad Mussa <105058756+GuyitsWALID@users.noreply.github.com>
2025-06-09 11:04:51 +0300

    Fix development server startup error

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,7 @@ const config = getDefaultConfig(__dirname);
 // Add font extensions to asset extensions
 config.resolver.assetExts.push('ttf', 'otf', 'woff', 'woff2');
 
+// Clear cache on startup
+config.resetCache = true;
+
 module.exports = config;
